a568289026aea9e857cdf616df5767e2afbb8472
Brett Tofel <btofel@redhat.com>
2022-02-15 15:00:14 -0500

    Makes GROUP different from DOMAIN

--- src/main/java/tech/tofel/TrinoOperator.java
+++ src/main/java/tech/tofel/TrinoOperator.java
@@ -8,7 +8,7 @@ import io.fabric8.kubernetes.model.annotation.Plural;
 import io.fabric8.kubernetes.model.annotation.Version;
 
 @Version("v1alpha1")
-@Group("tofel.tech.tofel.tech")
+@Group("trino-group.tofel.tech")
 @Kind("Trino-operator")
 @Plural("trino-operators")
 public class TrinoOperator extends CustomResource<TrinoOperatorSpec, TrinoOperatorStatus> implements Namespaced {}
