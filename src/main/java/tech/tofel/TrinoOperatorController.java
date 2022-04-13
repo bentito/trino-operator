5c2342441067fc472f8cb7155c7b88e8d6ea94aa
Brett Tofel <btofel@redhat.com>
2022-04-13 14:35:59 -0400

    Peg jdk image to specific 1.11 SHA

--- src/main/java/tech/tofel/TrinoOperatorController.java
+++ src/main/java/tech/tofel/TrinoOperatorController.java
@@ -1,5 +1,11 @@
 package tech.tofel;
 
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceBuilder;
+import io.fabric8.kubernetes.api.model.ServiceAccount;
+import io.fabric8.kubernetes.api.model.ServiceAccountBuilder;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
@@ -9,14 +15,24 @@ import io.javaoperatorsdk.operator.processing.event.source.polling.PerResourcePo
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
-@ControllerConfiguration(finalizerName = Constants.NO_FINALIZER)
+@ControllerConfiguration(finalizerName = Constants.NO_FINALIZER, namespaces = Constants.WATCH_CURRENT_NAMESPACE,
+    name="trinooperator")
 public class TrinoOperatorController implements Reconciler<TrinoOperator>, ErrorStatusHandler<TrinoOperator>,
         EventSourceInitializer<TrinoOperator> {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final KubernetesClient client;
+
+    public TrinoOperatorController(KubernetesClient client) {
+        this.client = client;
+
+    }
+
     @Override
     public List<EventSource> prepareEventSources(
             EventSourceContext<TrinoOperator> context) {
@@ -39,10 +55,6 @@ public class TrinoOperatorController implements Reconciler<TrinoOperator>, Error
     public Optional<TrinoOperator> updateErrorStatus(TrinoOperator schema, RetryInfo retryInfo,
                                                      RuntimeException e) {
         return Optional.empty();
-    }
-
-    public TrinoOperatorController() {
-        log.info("Starting operator.");
     }
 }
 
