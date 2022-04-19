ff469cefdfad941739aae28bb2a9f92d7d76feea
Brett Tofel <btofel@redhat.com>
2022-04-19 14:48:26 -0400

    Working trino operand deploy, auth err in prod

--- src/main/java/tech/tofel/TrinoOperatorController.java
+++ src/main/java/tech/tofel/TrinoOperatorController.java
@@ -1,31 +1,25 @@
 package tech.tofel;
 
-import io.fabric8.kubernetes.api.model.Namespace;
-import io.fabric8.kubernetes.api.model.NamespaceBuilder;
-import io.fabric8.kubernetes.api.model.ServiceAccount;
-import io.fabric8.kubernetes.api.model.ServiceAccountBuilder;
-import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.reconciler.*;
-import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.processing.event.EventSourceManager;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
-import io.javaoperatorsdk.operator.processing.event.source.polling.PerResourcePollingEventSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @ControllerConfiguration(finalizerName = Constants.NO_FINALIZER, namespaces = Constants.WATCH_CURRENT_NAMESPACE,
-    name="trinooperator")
+        name = "trinooperator")
 public class TrinoOperatorController implements Reconciler<TrinoOperator>, ErrorStatusHandler<TrinoOperator>,
         EventSourceInitializer<TrinoOperator> {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+    static final String APP_LABEL = "app.kubernetes.io/name";
     private final KubernetesClient client;
 
     public TrinoOperatorController(KubernetesClient client) {
@@ -42,6 +36,30 @@ public class TrinoOperatorController implements Reconciler<TrinoOperator>, Error
     @Override
     public UpdateControl<TrinoOperator> reconcile(TrinoOperator schema, Context context) {
         log.info("Reconciling update: {}", schema.getMetadata().getName());
+        final var name = schema.getMetadata().getName();
+        final var labels = Map.of(APP_LABEL, schema.getMetadata().getName());
+        final var spec = schema.getSpec();
+        final var imageRef = spec.getImageRef();
+
+        var deployment = new DeploymentBuilder()
+                .withMetadata(createMetadata(schema, labels))
+                .withNewSpec()
+                .withNewSelector().withMatchLabels(labels).endSelector()
+                .withNewTemplate()
+                .withNewMetadata().withLabels(labels).endMetadata()
+                .withNewSpec()
+                .addNewContainer()
+                .withName(name).withImage(imageRef)
+                .addNewPort()
+                .withName("http").withProtocol("TCP").withContainerPort(8080)
+                .endPort()
+                .endContainer()
+                .endSpec()
+                .endTemplate()
+                .endSpec()
+                .build();
+
+        client.apps().deployments().createOrReplace(deployment);
         return UpdateControl.noUpdate();
     }
 
@@ -55,6 +73,20 @@ public class TrinoOperatorController implements Reconciler<TrinoOperator>, Error
     public Optional<TrinoOperator> updateErrorStatus(TrinoOperator schema, RetryInfo retryInfo,
                                                      RuntimeException e) {
         return Optional.empty();
+    }
+
+    private ObjectMeta createMetadata(TrinoOperator resource, Map<String, String> labels) {
+        final var metadata = resource.getMetadata();
+        return new ObjectMetaBuilder()
+                .withName(metadata.getName())
+                .addNewOwnerReference()
+                .withUid(metadata.getUid())
+                .withApiVersion(resource.getApiVersion())
+                .withName(metadata.getName())
+                .withKind(resource.getKind())
+                .endOwnerReference()
+                .withLabels(labels)
+                .build();
     }
 }
 
--- src/main/java/tech/tofel/TrinoOperatorSpec.java
+++ src/main/java/tech/tofel/TrinoOperatorSpec.java
@@ -2,5 +2,10 @@ package tech.tofel;
 
 public class TrinoOperatorSpec {
 
+    public String getImageRef() {
+        return imageRef;
+    }
+
     // Add Spec information here
+    String imageRef;
 }
